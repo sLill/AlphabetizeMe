0581c8b81200229e03b25f9d69a9a9dc9c1e9c33
sLill <SamuelTurnerLill@gmail.com>
2023-01-05 08:47:37 -0600

    -

--- AlphabetizeMe/AlphabetizeMe/Commands/AlphabetizeCommand.cs
+++ AlphabetizeMe/AlphabetizeMe/Commands/AlphabetizeCommand.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.ComponentModelHost;
+﻿using Community.VisualStudio.Toolkit;
+using EnvDTE;
+using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Operations;
@@ -15,9 +17,11 @@ namespace AlphabetizeMe
     internal sealed class AlphabetizeCommand : BaseCommand<AlphabetizeCommand>
     {
         #region Fields..
-        private const string METHOD_PATTERN = @"(?<MethodText>(private|public|protected|internal).*?(?<MethodName>\w*?[^\b])\s*?\(.*?\).*?{(?>{(?<c>)|[^{}]+|}(?<-c>))*(?(c)(?!))})";
+        private const string METHOD_PATTERN = @"(?<MethodText>(private|public|protected|internal)[\s\w]*?(?<MethodName>\w*?[^\b])\s*?\(.*?\).*?{(?>{(?<c>)|[^{}]+|}(?<-c>))*(?(c)(?!))})";
+        private const string PROPERTY_PATTERN = @"(?<PropertyText>(public|private|internal|protected)(?!\(.*?\))*?[\w\s]*?(?<PropertyName>[\w]+?)\s*?({(?>{(?<c>)|[^{}]+|}(?<-c>))*(?(c)(?!))}|(=[^{}]*?)*;))";
 
         private static Regex _methodRegex;
+        private static Regex _propertyRegex;
         #endregion Fields..
 
         #region Structs..
@@ -27,49 +31,100 @@ namespace AlphabetizeMe
             public string Text;
             public int StartIndex;
         }
+
+        public struct Property
+        {
+            public string Name;
+            public string Text;
+            public int StartIndex;
+        }
         #endregion Structs..
 
+        #region Constructors..
+        public AlphabetizeCommand()
+        {
+            Initialize();
+        }
+        #endregion Constructors..
+
         #region Methods..
+        private void Initialize()
+        {
+            _methodRegex = new Regex(METHOD_PATTERN, RegexOptions.Singleline);
+            _propertyRegex = new Regex(PROPERTY_PATTERN, RegexOptions.Singleline);
+        }
+
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            _methodRegex = _methodRegex ?? new Regex(METHOD_PATTERN, RegexOptions.Singleline);
-
             var documentView = await VS.Documents.GetActiveDocumentViewAsync();
             var selections = documentView?.TextView.Selection.SelectedSpans.ToList() ?? new List<SnapshotSpan>();
 
-            List<Method> selectionMethods; 
             selections.ForEach(selection =>
             {
-                selectionMethods = new List<Method>();
-
-                // Extract methods from selected text
-                var matches = _methodRegex.Matches(selection.GetText());
-                foreach (Match match in matches)
-                {
-                    var methodName = match.Groups["MethodName"].Value;
-                    var methodText = match.Groups["MethodText"].Value;
-                    var methodIndex = match.Groups["MethodText"].Index;
-
-                    selectionMethods.Add(new Method() 
-                    { 
-                        Name = methodName, 
-                        Text = methodText,
-                        StartIndex = selection.Start + methodIndex
-                    });
-                }
-
-                // Sort
-                var sortedSelectionMethods = selectionMethods.OrderBy(x => x.Name).ToList();
-
-                // Replace
                 var textEdit = documentView.TextBuffer.CreateEdit();
 
-                for (int i = 0; i < selectionMethods.Count; i++)
-                    textEdit.Replace(selectionMethods[i].StartIndex, selectionMethods[i].Text.Length, sortedSelectionMethods[i].Text);
+                AlphabetizeMethods(textEdit, selection);
+                AlphabetizeProperties(textEdit, selection);
 
                 textEdit.Apply();
             });
         } 
+
+        private void AlphabetizeMethods(ITextEdit textEdit, SnapshotSpan selection)
+        {
+            var selectionMethods = new List<Method>();
+
+            // Extract methods from selected text
+            var matches = _methodRegex.Matches(selection.GetText());
+            foreach (Match match in matches)
+            {
+                var methodName = match.Groups["MethodName"].Value;
+                var methodText = match.Groups["MethodText"].Value;
+                var methodIndex = match.Groups["MethodText"].Index;
+
+                selectionMethods.Add(new Method()
+                {
+                    Name = methodName,
+                    Text = methodText,
+                    StartIndex = selection.Start + methodIndex
+                });
+            }
+
+            // Sort
+            var sortedSelectionMethods = selectionMethods.OrderBy(x => x.Name).ToList();
+
+            // Replace
+            for (int i = 0; i < selectionMethods.Count; i++)
+                textEdit.Replace(selectionMethods[i].StartIndex, selectionMethods[i].Text.Length, sortedSelectionMethods[i].Text);
+        }
+
+        private void AlphabetizeProperties(ITextEdit textEdit, SnapshotSpan selection)
+        {
+            var selectionProperties = new List<Property>();
+
+            // Extract methods from selected text
+            var matches = _propertyRegex.Matches(selection.GetText());
+            foreach (Match match in matches)
+            {
+                var propertyName = match.Groups["PropertyName"].Value;
+                var propertyText = match.Groups["PropertyText"].Value;
+                var propertyIndex = match.Groups["PropertyText"].Index;
+
+                selectionProperties.Add(new Property()
+                {
+                    Name = propertyName,
+                    Text = propertyText,
+                    StartIndex = selection.Start + propertyIndex
+                });
+            }
+
+            // Sort
+            var sortedSelectionProperties = selectionProperties.OrderBy(x => x.Name).ToList();
+
+            // Replace
+            for (int i = 0; i < selectionProperties.Count; i++)
+                textEdit.Replace(selectionProperties[i].StartIndex, selectionProperties[i].Text.Length, sortedSelectionProperties[i].Text);
+        }
         #endregion Methods..
     }
 }
